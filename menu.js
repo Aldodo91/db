be2690d66b5bbf08abba7f2698a3a1b08c8186a7
Aldodo <45825599+Aldodo91@users.noreply.github.com>
2022-08-22 22:15:45 +0200

    ok

--- menu.js
+++ menu.js
@@ -1,33 +1,4 @@
-/*
-  Ordine : 
-  - signature 
-  - Sprits
-  - mocktail
-  - vini (bianchi, rose, rossi, spumanti);
-  - // distillati (Gin, Vodka, Rum, Tequila, Scoth Whisky, Bourbon Whiskey, Rye Whiskey, Japanese Whisky, Irish Whisky, Cognac, Armagnac,) 
-  - softDrink
-  - Caffetteria 
-  - Food
-
-  Ordine Sidebad: 
-  - Signature 
-  - Mocktail 
-  - Vini | Wine 
-  - Distillati | Spirits
-  - Soft Drink
-  - Caffetteria | Coffee
-  - Food 
-*/
-toShare = [
-  // immagine Biancha vuota (cambia nome della categoria ex spriz)
-  // {
-  //   img: null,
-  //   nome: "Racy Spritz",
-  //   ricetta: {
-  //     it: "Racy Aperitivo, Prosecco, Frutta fresca",
-  //     en: "Racy Aperitivo, Prosecco, Fresh fruit",
-  //   },
-  // },
+[
   {
     img: null,
     nome: "50 Shades of Grey Goose",
